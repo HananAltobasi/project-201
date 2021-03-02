e84ad4b7788799a72ab18060311096e259f11409
hanan omar altobasi <hananaltobase96@gmail.com>
2021-03-02 08:22:22 +0200

    creat new files, I thought that the work would take more than 4 hours, but it took 3 hours, some problems happened, but it was solved, things went right, and I learned something new.

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+let myName='hanan';
+let myBirthday='may/6';
+let favcolor='pink';
+let favTypeRose='tulib';
+let myTalent='writing';
+
+
+let userName=prompt('welcom on my website, what is your name?');
+alert('Hello ' + userName + " Answer with yes or no");
+
+let name =prompt('my name is huda?');
+if (name===no) {
+    alert('that is true, my name is hanan')
+} else {
+    alert('that is false, my name is hanan')
+}
+
+let birthday =prompt('my Birthday is may/6?');
+if (birthday===yes) {
+    alert('that is true')
+} else {
+    alert('that is false, my Birthday is may/6')
+}
+
+let color =prompt('my fav Color is red?');
+if (color===no) {
+    alert('that is true; my favcolor is pink')
+} else {
+    alert('that is false, my favcolor is pink')
+}
+
+let typerose =prompt('my TypeRose is jasmine?');
+if (typerose===no) {
+    alert('that is true, my favrose is tulip')
+} else {
+    alert('that is false, my favrose is tulip')
+}
+
+let talent =prompt('my Talent is writing?');
+if (talent===yes) {
+    alert('that is true')
+} else {
+    alert('that is false, my Talent is writing')
+}
+
+console.log(name,birthday,color,typerose,talent);
+
+
+
+alert('We hope you had a great time with us ' + userName + " Come back soon");
+
